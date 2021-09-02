9d850a0f42546fff87f4a15170723573f2b2dea9
USER <bluelove113@outlook.com>
2021-09-02 20:07:38 +0530

    data types

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-console.log("Hello everyone , my first js piece")
-console.log({name: "Biprajit"})
+// Data Types
+// Primitive and nonprimitive 
 
-console.warn("this is a warning")
-console.error("This is an error")
+let age = 19;
+console.log(age)
+console.log(typeof age)
 
-console.error("This is an error")
+let brand
+console.log(brand)
+
+let Name ="Biprock";
+console.log(Name)
+console.log(typeof Name)
+
+
+//non-primitive
+
+let arr = [1,2,3,4,5]
+arr.push("hello")
+arr.unshift(true)
+console.log(arr)
+console.log(typeof arr)
