e5707d52a2b6380db9748b8a19d83625794b448b
USER <bluelove113@outlook.com>
2021-09-02 19:28:51 +0530

    console

---
+++ app.js
@@ -0,0 +1,7 @@
+console.log("Hello everyone , my first js piece")
+console.log({name: "Biprajit"})
+
+console.warn("this is a warning")
+console.error("This is an error")
+
+console.error("This is an error")
