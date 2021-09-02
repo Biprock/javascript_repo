e2bc20d1e8e71c8d517fe15009706a3aeb5aed91
USER <bluelove113@outlook.com>
2021-09-02 20:16:42 +0530

    todays1

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 // Data Types
 // Primitive and nonprimitive 
 
-let age = 19;
-console.log(age)
-console.log(typeof age)
 
 let brand
 console.log(brand)
 
-let Name ="Biprock";
-console.log(Name)
-console.log(typeof Name)
 
 
 //non-primitive
@@ -20,3 +14,12 @@ arr.push("hello")
 arr.unshift(true)
 console.log(arr)
 console.log(typeof arr)
+
+
+
+let Name = "Biprajit"
+let age=19
+console.log(Name.toUpperCase())
+
+
+// coersion
