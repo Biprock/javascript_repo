1fe446f1cc4910188da152aade1d2449249afed3
USER <bluelove113@outlook.com>
2021-09-03 19:37:24 +0530

    arithmetic operators

--- app.js
+++ app.js
@@ -21,13 +21,34 @@ let Name = "Biprajit"
 let age=19
 console.log(Name.toUpperCase())
 
+//const name = "Biprajit"
+
+
 
 // coersion in js 
 
 
 // Arithmetic operators
+  const a = 10
+  const b = 31
+  console.log(a+b)
+  console.log(a-b)
+  console.log(a*b)
+  console.log(a/b)
+  console.log(b%a)
 
-const name = "Biprajit"
 
-name = "Biprock"
-console.log(name)
+console.log('----------------------------------------------------')
+
+// Assignment operators
+
+
+  let c = 0 
+  let d = 5
+  let e = 5
+  c++ //c=c+1
+  d*=5
+  e**=5
+  console.log(c)
+  console.log(d)
+  console.log(e)
