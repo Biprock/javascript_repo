09803a470dc43283befdce82b7faae8656a71a09
USER <bluelove113@outlook.com>
2021-09-06 18:20:47 +0530

    function keyword

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
-const rock = document.querySelector('#rock')
+//const rock = document.querySelector('#rock')
 
-const paper = document.querySelector('#paper')
+//const paper = document.querySelector('#paper')
 
 
-const scissor = document.querySelector('#scissor')
+//const scissor = document.querySelector('#scissor')
+
+function LogName(name)
+{
+    //console.log("Biprajit")
+    console.log(name)
+}
+
+LogName( )
+
+
+function add(a=0,b=0)
+{
+    const sum = a+b
+    return sum
+}
+const result = add(12,99)
+console.log(result)
