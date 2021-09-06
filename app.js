0a2efca622fb7f2736732444343328d59a7040da
USER <bluelove113@outlook.com>
2021-09-06 17:54:04 +0530

    rps

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const rock = document.querySelector('#rock')
-console.log(rock)
 
 const paper = document.querySelector('#paper')
-console.log(paper)
+
 
 const scissor = document.querySelector('#scissor')
-console.log(scissor)
